7d781130c4f1d80505e9172db3329f26afe57b00
yossisp1972 <46672457+yossisp1972@users.noreply.github.com>
2026-09-15 15:16:45 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -124,6 +124,19 @@ resource "aws_iam_role" "ec2" {
   })
 }
 
+resource "aws_ecr_repository" "demo_app" {
+  name                 = "demo-app"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "demo-app"
+  }
+}
+
 resource "aws_iam_instance_profile" "ec2" {
   name = "k3s-lab-instance-profile"
   role = aws_iam_role.ec2.name
