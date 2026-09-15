2fdf481b4434405b01741c2a4fafcb380490f505
yossisp1972 <46672457+yossisp1972@users.noreply.github.com>
2026-09-15 10:44:30 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,17 +16,9 @@ variable "instance_type" {
   default     = "t3.medium"
 }
 
-variable "allowed_ssh_cidr" {
-  description = "CIDR allowed to SSH to the EC2 instance"
-  type        = string
-
   validation {
     condition     = var.allowed_ssh_cidr != "0.0.0.0/0"
     error_message = "Do not expose SSH to the entire internet. Supply your public IP as x.x.x.x/32."
   }
 }
 
-variable "public_key" {
-  description = "SSH public key used to access the K3s EC2 instance"
-  type        = string
-}
