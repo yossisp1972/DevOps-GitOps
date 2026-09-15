7665cedbe0ef6063cb497a07805d95c9a649b49e
yossisp1972 <46672457+yossisp1972@users.noreply.github.com>
2026-09-15 10:52:06 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -135,7 +135,6 @@ resource "aws_instance" "k3s" {
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.k3s.id]
 
-  key_name             = aws_key_pair.k3s.key_name
   iam_instance_profile = aws_iam_instance_profile.ec2.name
 
   root_block_device {
